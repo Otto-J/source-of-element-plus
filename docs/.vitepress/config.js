547fe0cfaad11257c56e8c5e732927b3da05a578
Otto <fa@ijust.cc>
2021-11-06 07:40:01 +0800

    ci: 补充 actions 部署 pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ const data = require("./data.json");
 module.exports = {
   title: "分析 element-plus 源码",
   description: "人人都能看懂",
-  base: "/",
+  base: "/source-of-element-plus",
   lang: "zh-CN",
   themeConfig: {
     repo: "Otto-J/source-of-element-plus",
